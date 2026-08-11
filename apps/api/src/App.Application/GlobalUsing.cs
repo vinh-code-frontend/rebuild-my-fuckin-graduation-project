85fe0d3af900fcfdbfd8f54a62eec77350ca5ba9
Nguyen Le Vinh <79951174+vinh-code-frontend@users.noreply.github.com>
2026-08-11 17:41:56 +0700

    validate: add fluent validator

--- apps/api/src/App.Application/GlobalUsing.cs
+++ apps/api/src/App.Application/GlobalUsing.cs
@@ -4,3 +4,4 @@ global using App.Application.Repositories;
 global using App.Application.DTOs;
 global using App.Domain.Entities;
 global using Microsoft.AspNetCore.Http;
+global using FluentValidation;
